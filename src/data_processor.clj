7a2e34ff43771f1c47ea662247a1a90a95de0627
maxi <maxiejbe92@gmail.com>
2018-04-01 19:11:00 -0300

    Initializing counters from define-counter rules

--- src/data_processor.clj
+++ src/data_processor.clj
@@ -1,7 +1,19 @@
 (ns data-processor)
 
+(defn define-counter [params]
+	(zipmap [:name :subcounters :condition] params))
+
+(defmulti define-rule 
+  (fn[function params] (function "function")))
+
+(defmethod define-rule "define-counter" [function, params]         
+    (define-counter params))
+  
+(defn evaluate-function [f] 
+	(define-rule {"function" (str (first f))} (rest f)))  
+
 (defn initialize-processor [rules]
-  nil)
+  (map evaluate-function rules))
          
 (defn process-data [state new-data]
   [nil []])
