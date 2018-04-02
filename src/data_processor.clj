aab2cd2eb5f9da1628a6e251ee058a7e7e33a644
maxi <maxiejbe92@gmail.com>
2018-04-01 23:11:27 -0300

    Including define-signal method

--- src/data_processor.clj
+++ src/data_processor.clj
@@ -3,12 +3,23 @@
 (defn define-counter [params]
 	(zipmap [:name :subcounters :condition] params))
 
+(defn define-signal [params]
+	( let [
+			name (first (keys (first params)))
+			expression (first (vals (first params)))
+			condition (last params)
+		] 
+	(zipmap [:name :expression :condition] [name expression condition])))
+
 (defmulti define-rule 
   (fn[function params] (function "function")))
 
 (defmethod define-rule "define-counter" [function, params]         
     (define-counter params))
-  
+
+(defmethod define-rule "define-signal" [function, params]         
+    (define-signal params))
+
 (defn evaluate-function [f] 
 	(define-rule {"function" (str (first f))} (rest f)))  
 
