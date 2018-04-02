681831e20690b6e57ef0f730e02838d7df5c2d19
maxi <maxiejbe92@gmail.com>
2018-04-01 23:19:03 -0300

    Moving rules to a separated file

--- src/data_processor.clj
+++ src/data_processor.clj
@@ -1,27 +1,5 @@
 (ns data-processor)
-
-(defn define-counter [params]
-	(zipmap [:name :subcounters :condition] params))
-
-(defn define-signal [params]
-	( let [
-			name (first (keys (first params)))
-			expression (first (vals (first params)))
-			condition (last params)
-		] 
-	(zipmap [:name :expression :condition] [name expression condition])))
-
-(defmulti define-rule 
-  (fn[function params] (function "function")))
-
-(defmethod define-rule "define-counter" [function, params]         
-    (define-counter params))
-
-(defmethod define-rule "define-signal" [function, params]         
-    (define-signal params))
-
-(defn evaluate-function [f] 
-	(define-rule {"function" (str (first f))} (rest f)))  
+(use 'rules)
 
 (defn initialize-processor [rules]
   (map evaluate-function rules))
